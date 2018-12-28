179f17a471a9659737c48796ca702a9cba1dbc71
Jacob Feldman <jacobgfeld@gmail.com>
2018-12-28 20:49:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Triangle
   attr_reader :kind
   
@@ -5,7 +7,15 @@ class Triangle
     @s1 = s1; @s2 = s2; @s3 = s3;
     
     if !triangleIsValid?
+<<<<<<< HEAD
       raise TriangleError
+=======
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.invalidSize
+      end
+>>>>>>> 836aa86a1dfd1bae12892ce1ec14a8a7924c55b7
     else
       setTriangleType
     end
@@ -25,6 +35,7 @@ class Triangle
   end
   
   def triangleIsValid?
+<<<<<<< HEAD
     return false if (@s1 <= 0 || @s2 <= 0 || @s3 <= 0)
     
     if ((@s1+@s2 <= @s3) ||
@@ -34,9 +45,27 @@ class Triangle
         return false
     end
     
+=======
+    if (@s1 == 0 || @s2 == 0 || @s3 == 0)
+      return false
+      
+    if ((@s1+@s2 > @s3) ||
+        (@s1+@s3 > @s2) ||
+        (@s2+@s3 > @s3)) 
+      then
+        return false
+      end
+    end
+>>>>>>> 836aa86a1dfd1bae12892ce1ec14a8a7924c55b7
     return true
   end
 
   class TriangleError < StandardError
+<<<<<<< HEAD
+=======
+    def invalidSize
+      "Triangle is invalid"
+    end
+>>>>>>> 836aa86a1dfd1bae12892ce1ec14a8a7924c55b7
   end
 end
